987cfb502cbae6fd17f8c5b4245d41bd212fc43c
Quillraven <quillraven@gmail.com>
2024-03-14 22:12:50 +0100

    add collision handling support for any Tiled shape

--- assets/maps/objects.tsx
+++ assets/maps/objects.tsx
@@ -11,6 +11,18 @@
    <object id="6" x="8" y="15" width="17" height="17">
     <ellipse/>
    </object>
+   <object id="7" x="7" y="2" width="18" height="6">
+    <ellipse/>
+   </object>
+   <object id="8" x="4" y="3">
+    <polygon points="0,0 -3,3 -2,9 -1,5"/>
+   </object>
+   <object id="9" x="27" y="4">
+    <polygon points="0,0 0,14 2,6"/>
+   </object>
+   <object id="10" x="1" y="15">
+    <polyline points="0,0 6,17"/>
+   </object>
   </objectgroup>
  </tile>
 </tileset>
--- core/src/main/kotlin/com/quillraven/github/quillyjumper/b2dUtils.kt
+++ core/src/main/kotlin/com/quillraven/github/quillyjumper/b2dUtils.kt
@@ -2,48 +2,118 @@ package com.quillraven.github.quillyjumper
 
 import com.badlogic.gdx.maps.MapObject
 import com.badlogic.gdx.maps.objects.EllipseMapObject
+import com.badlogic.gdx.maps.objects.PolygonMapObject
+import com.badlogic.gdx.maps.objects.PolylineMapObject
 import com.badlogic.gdx.maps.objects.RectangleMapObject
-import com.badlogic.gdx.math.MathUtils
+import com.badlogic.gdx.math.MathUtils.*
+import com.badlogic.gdx.physics.box2d.ChainShape
 import com.badlogic.gdx.physics.box2d.CircleShape
 import com.badlogic.gdx.physics.box2d.FixtureDef
-import com.badlogic.gdx.physics.box2d.PolygonShape
 import com.quillraven.github.quillyjumper.Quillyjumper.Companion.UNIT_SCALE
 import ktx.app.gdxError
 import ktx.math.vec2
+import ktx.tiled.x
+import ktx.tiled.y
 
 fun fixtureDefOf(mapObject: MapObject): FixtureDef {
     return when (mapObject) {
-        is RectangleMapObject -> {
-            val (rectX, rectY, rectW, rectH) = mapObject.rectangle
-            val boxW = rectW * UNIT_SCALE / 2f
-            val boxH = rectH * UNIT_SCALE / 2f
-            val boxOffset = vec2(
-                rectX * UNIT_SCALE + boxW,
-                rectY * UNIT_SCALE + boxH
-            )
-
-            FixtureDef().apply {
-                shape = PolygonShape().apply { setAsBox(boxW, boxH, boxOffset, 0f) }
-            }
-        }
-
-        is EllipseMapObject -> {
-            val (x, y, w, h) = mapObject.ellipse
-            val ellipseW = w * UNIT_SCALE / 2f
-            val ellipseH = h * UNIT_SCALE / 2f
-
-            if (!MathUtils.isEqual(ellipseW, ellipseH, 0.1f)) {
-                gdxError("Ellipse shape is only supported if the width and height are equal")
-            }
-
-            FixtureDef().apply {
-                shape = CircleShape().apply {
-                    position = vec2(x * UNIT_SCALE + ellipseW, y * UNIT_SCALE + ellipseH)
-                    radius = ellipseW
-                }
-            }
-        }
-
+        is RectangleMapObject -> rectangleFixtureDef(mapObject)
+        is EllipseMapObject -> ellipseFixtureDef(mapObject)
+        is PolygonMapObject -> polygonFixtureDef(mapObject)
+        is PolylineMapObject -> polylineFixtureDef(mapObject)
         else -> gdxError("Unsupported MapObject $mapObject")
+    }
+}
+
+private fun polylineFixtureDef(mapObject: PolylineMapObject): FixtureDef {
+    return polygonFixtureDef(mapObject.x, mapObject.y, mapObject.polyline.vertices, false)
+}
+
+private fun polygonFixtureDef(mapObject: PolygonMapObject): FixtureDef {
+    return polygonFixtureDef(mapObject.x, mapObject.y, mapObject.polygon.vertices, true)
+}
+
+private fun polygonFixtureDef(
+    polyX: Float,
+    polyY: Float,
+    polyVertices: FloatArray,
+    loop: Boolean,
+): FixtureDef {
+    val x = polyX * UNIT_SCALE
+    val y = polyY * UNIT_SCALE
+    val vertices = FloatArray(polyVertices.size) { vertexIdx ->
+        if (vertexIdx % 2 == 0) {
+            x + polyVertices[vertexIdx] * UNIT_SCALE
+        } else {
+            y + polyVertices[vertexIdx] * UNIT_SCALE
+        }
+    }
+
+    return FixtureDef().apply {
+        shape = ChainShape().apply {
+            if (loop) {
+                createLoop(vertices)
+            } else {
+                createChain(vertices)
+            }
+        }
+    }
+}
+
+private fun ellipseFixtureDef(mapObject: EllipseMapObject): FixtureDef {
+    val (x, y, w, h) = mapObject.ellipse
+    val ellipseX = x * UNIT_SCALE
+    val ellipseY = y * UNIT_SCALE
+    val ellipseW = w * UNIT_SCALE / 2f
+    val ellipseH = h * UNIT_SCALE / 2f
+
+    return if (isEqual(ellipseW, ellipseH, 0.1f)) {
+        // width and height are equal -> return a circle shape
+        FixtureDef().apply {
+            shape = CircleShape().apply {
+                position = vec2(ellipseX + ellipseW, ellipseY + ellipseH)
+                radius = ellipseW
+            }
+        }
+    } else {
+        // width and height are not equal -> return an ellipse shape (=polygon with 'numVertices' vertices)
+        val numVertices = 20
+        val angleStep = PI2 / numVertices
+        val vertices = Array(numVertices) { vertexIdx ->
+            val angle = vertexIdx * angleStep
+            val offsetX = ellipseW * cos(angle)
+            val offsetY = ellipseH * sin(angle)
+            vec2(ellipseX + ellipseW + offsetX, ellipseY + ellipseH + offsetY)
+        }
+
+        FixtureDef().apply {
+            shape = ChainShape().apply {
+                createLoop(vertices)
+            }
+        }
+    }
+}
+
+// box is centered around body position in Box2D, but we want to have it aligned in a way
+// that the body position is the bottom left corner of the box.
+// That's why we use a 'boxOffset' below.
+private fun rectangleFixtureDef(mapObject: RectangleMapObject): FixtureDef {
+    val (rectX, rectY, rectW, rectH) = mapObject.rectangle
+    val boxX = rectX * UNIT_SCALE
+    val boxY = rectY * UNIT_SCALE
+    val boxW = rectW * UNIT_SCALE
+    val boxH = rectH * UNIT_SCALE
+
+    val vertices = arrayOf(
+        vec2(boxX, boxY),
+        vec2(boxX + boxW, boxY),
+        vec2(boxX + boxW, boxY + boxH),
+        vec2(boxX, boxY + boxH),
+    )
+
+    return FixtureDef().apply {
+        shape = ChainShape().apply {
+            createLoop(vertices)
+        }
     }
 }
