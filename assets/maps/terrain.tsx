357b1603f736985b1dc430d5922163696085ecfd
Quillraven <quillraven@gmail.com>
2024-03-14 22:17:41 +0100

    rename terrain_extruded to terrain

--- assets/maps/terrain.tsx
+++ assets/maps/terrain.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="terrain_extruded" tilewidth="16" tileheight="16" spacing="8" margin="4" tilecount="242" columns="22">
- <image source="../graphics/terrain_extruded.png" width="528" height="264"/>
+<tileset version="1.10" tiledversion="1.10.2" name="terrain" tilewidth="16" tileheight="16" spacing="8" margin="4" tilecount="242" columns="22">
+ <image source="../graphics/terrain.png" width="528" height="264"/>
  <tile id="6">
   <objectgroup draworder="index" id="2">
    <object id="1" x="2" y="3" width="14" height="10"/>
