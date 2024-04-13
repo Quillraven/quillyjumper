1f66dab5ff1a05c74859ba5dc76ca168e3ce5a9a
Quillraven <quillraven@gmail.com>
2024-04-13 16:52:00 +0000

    Deploying to gh-pages from @ Quillraven/quillyjumper@e464bb3ee65562aab1035a828381be8ace0dbb8d 🚀

--- assets/maps/terrain.tsx
+++ assets/maps/terrain.tsx
@@ -1,6 +1,21 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="terrain" tilewidth="16" tileheight="16" spacing="8" margin="4" tilecount="242" columns="22">
  <image source="../graphics/terrain.png" width="528" height="264"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="3" width="15" height="13"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="3" width="15" height="5"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="3" width="15" height="13"/>
+  </objectgroup>
+ </tile>
  <tile id="6">
   <objectgroup draworder="index" id="2">
    <object id="1" x="2" y="3" width="14" height="7"/>
@@ -16,6 +31,31 @@
    <object id="1" x="0" y="3" width="14" height="7"/>
   </objectgroup>
  </tile>
+ <tile id="22">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="1" width="7" height="15"/>
+  </objectgroup>
+ </tile>
+ <tile id="24">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="8" y="1" width="7" height="15"/>
+  </objectgroup>
+ </tile>
+ <tile id="44">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="1" width="14" height="14"/>
+  </objectgroup>
+ </tile>
+ <tile id="45">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="8" width="15" height="7"/>
+  </objectgroup>
+ </tile>
+ <tile id="46">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="1" width="15" height="14"/>
+  </objectgroup>
+ </tile>
  <tile id="94">
   <objectgroup draworder="index" id="2">
    <object id="1" x="2" y="3" width="14" height="7"/>
@@ -29,6 +69,21 @@
  <tile id="96">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="3" width="14" height="7"/>
+  </objectgroup>
+ </tile>
+ <tile id="103">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="3" width="14" height="13"/>
+  </objectgroup>
+ </tile>
+ <tile id="125">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="0" width="14" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="147">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="0" width="14" height="15"/>
   </objectgroup>
  </tile>
  <wangsets>
