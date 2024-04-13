bd568a904a624c455eed25cc396168821efa3a3f
Quillraven <quillraven@gmail.com>
2024-04-13 17:53:08 +0200

    add map 1

--- assets/maps/terrain.tsx
+++ assets/maps/terrain.tsx
@@ -16,6 +16,21 @@
    <object id="1" x="0" y="3" width="14" height="7"/>
   </objectgroup>
  </tile>
+ <tile id="94">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="2" y="3" width="14" height="7"/>
+  </objectgroup>
+ </tile>
+ <tile id="95">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="3" width="16" height="7"/>
+  </objectgroup>
+ </tile>
+ <tile id="96">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="3" width="14" height="7"/>
+  </objectgroup>
+ </tile>
  <wangsets>
   <wangset name="Unbenannter Satz" type="corner" tile="-1"/>
  </wangsets>
